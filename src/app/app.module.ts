910d11635c8d40cec2593f942267e55479fe999d
Rodrigo_Roman <contacto@undel.cl>
2018-05-13 19:05:34 -0400

    login funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginformComponent } from './loginform/loginform.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes:Routes = [
+  {
+    path: '',
+    component: LoginformComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },  
+
+
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginformComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
