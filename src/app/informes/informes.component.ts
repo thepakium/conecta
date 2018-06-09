72f0d4777849921a878d7607ef520a5fd3f4366c
Rodrigo_Roman <contacto@undel.cl>
2018-06-09 18:59:35 -0400

    grafico de pie

--- src/app/informes/informes.component.ts
+++ src/app/informes/informes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ChartsModule } from 'ng2-charts';
 import { BusquedaService } from '../share/busqueda.service';
 import { environment } from '../../environments/environment';
+import { getLocaleDateFormat } from '@angular/common';
 
 @Component({
   selector: 'app-informes',
@@ -11,52 +12,73 @@ import { environment } from '../../environments/environment';
 })
 
 export class InformesComponent implements OnInit {
-  datarecibida = [];
-  Contador_sms_mes: Conta[];
-  sugerencias: any;
-  visible: false;
+  datarecibida: Estates[];
+  Contador_estates: Estates[];
   loadingIndicator = true;
-  public pieChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
-  public pieChartData: number[] = [300, 500, 100];
+  mes: number;
+  meses = [ {id: 1, name: 'Enero'},
+            {id: 2, name: 'Febrero'},
+            {id: 3, name: 'Marzo'},
+            {id: 4, name: 'Abril'},
+            {id: 5, name: 'Mayo'},
+            {id: 6, name: 'Junio'},
+            {id: 7, name: 'Julio'},
+            {id: 8, name: 'Agosto'},
+            {id: 9, name: 'Septiembre'},
+            {id: 10, name: 'Octubre'},
+            {id: 11, name: 'Noviembre'},
+            {id: 12, name: 'Diciembre'},
+          ]
+  
+  public pieChartLabels: string[] = [];
+  public pieChartData: number[] = [];
   public pieChartType: string = 'pie';
-
+  
+  Fechas_mes_to_php (){
+    let  fechaInicio = new Date( 2018, this.mes-1 , 1 ,0,0,1 ).toISOString().substr(0, 19).replace('T', ' ');
+    let fechaFinal = new Date( 2018, this.mes , 1 ,0,0,1).toISOString().substr(0, 19).replace('T', ' ');
+    console.log("F i",fechaInicio);
+    console.log("F F",fechaFinal);
+    this.buscarconteoestados(fechaInicio,fechaFinal)
+  }
   // events
   public chartClicked(e:any):void {
     console.log(e);
   }
- 
   public chartHovered(e:any):void {
     console.log(e);
   }
-
   constructor(private busquedaService: BusquedaService) {
     this.buscarMensajes();
-    this.buscarconteosms();
-
 }
+
   ngOnInit() {
   }
 
-  buscarconteosms() {
-      const datos = { tipo: 'contador_sms'};
+  buscarconteoestados(fecha1,fecha2) {
+      this.pieChartLabels=[];
+      const datos = { tipo: 'contador_estados',fechainicio: fecha1,fechafinal: fecha2};
       this.busquedaService.obtenerDatos( JSON.stringify(datos) , data => {
-        this.Contador_sms_mes = data;
-        console.log(this.Contador_sms_mes); } );
-  }
+        this.Contador_estates = data;
+        const cloneb = JSON.parse(JSON.stringify(this.pieChartData));
+        for (let i = 0; i < this.Contador_estates.length; i++){
+          cloneb[i] = this.Contador_estates[i].cuantose;
+          this.pieChartLabels.push(this.Contador_estates[i].estado);
+          }
+        this.pieChartData = cloneb;
+        } );
+      }
 
   buscarMensajes() {
       const datos = { tipo: 'mensajes'};
       this.busquedaService.obtenerDatos( JSON.stringify(datos) ,
               data => { this.datarecibida = data;
-                        setTimeout(() => { this.loadingIndicator = false; }, 1500); }
-     );
-  }
-
-
-
+              //console.log(this.datarecibida);
+              setTimeout(() => { this.loadingIndicator = false; }, 1500); });
+      }
 }
 
-
-interface Conta {
-  cuantos: string;
+interface Estates {
+  estado: string;
+  cuantose: string;
 }
