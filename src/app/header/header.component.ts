042dae729afc2ce9be347b5e7e0a00ee6dc78087
Rodrigo_Roman <contacto@undel.cl>
2018-06-10 19:14:54 -0400

    botones varios

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { BusquedaService } from '../share/busqueda.service';
+
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
   sugerencias = 'escriba aqui su comentario';
+  //el: angular.element('#divId');
+  //el.attr('attr-name', 'attr-value');
   constructor(private busquedaService: BusquedaService) { }
-
+  estado="btn btn-primary float-top-s";
   ngOnInit() {
+    this.retirarboton()
   }
  // send(): void { //ESPERANDO sendData
  //   this.busquedaService.sendSugerido(this.sugerencias)
  //     .subscribe(res => console.log(res));
- // }
+ moverin():void{
+   this.estado="btn btn-primary float-top-s"
+ }
+ moverout():void{
+   this.estado="btn btn-primary float-top-h"
+ }
+ retirarboton():void{
+  setTimeout(() => { this.moverout(); }, 1000);
+ }
 }
