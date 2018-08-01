ee86f1ed126bf54aa63db390e67cc33a41c7b8fb
Rodrigo_Roman <contacto@undel.cl>
2018-08-01 03:56:31 -0400

    molde en encabezado de cada hoja - salir vuelve a funcionar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component,OnInit } from '@angular/core';
-import { Usuario, Username } from 'src/app/share/models';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  logged: Username;
-  title = 'app';
-  constructor() { }
-
-  ngOnInit() {
-    this.logged = JSON.parse(localStorage.getItem('user'));
-  }
+export class AppComponent  {
 
 }
