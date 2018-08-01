b13ede3fa546f6caae7460e753f8ed14aa4ff984
Rodrigo_Roman <contacto@undel.cl>
2018-08-01 04:50:09 -0400

    logout funciona en sidebar

--- src/app/sidemenu/sidemenu.component.ts
+++ src/app/sidemenu/sidemenu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario, Username } from 'src/app/share/models';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
@@ -9,10 +10,14 @@ import { Usuario, Username } from 'src/app/share/models';
 export class SidemenuComponent implements OnInit {
   logged: Username;
 
-  constructor() { }
+  constructor(private router: Router,) { }
 
   ngOnInit() {
     this.logged = JSON.parse(localStorage.getItem('user'));
   }
-
+  logout() {
+    localStorage.removeItem('conecta2In');
+    localStorage.removeItem('user');
+    this.router.navigate(['/login']);
+  }
 }
