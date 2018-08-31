3f3635ff7479de592918a0c8fdfb2f1ae1ee1270
Alexis Gonzalez <agonzaleza@sernapesca.cl>
2018-08-31 13:47:42 -0300

    despliegue de compras de sms en perfiles

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Usuario, Username } from 'src/app/share/models';
 import { BusquedaService } from 'src/app/share/busqueda.service';
 import { NgbModal, NgbActiveModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -16,8 +17,10 @@ export class ProfileComponent implements OnInit {
   loadingIndicator = true;
   cantidades: Dato[];
   cambio = {} as Clave;
+  compras = [] as Compra[];
   private modalGestionRef: NgbModalRef;
   private modal: NgbActiveModal;
+  public cuota$: Observable<Number>;
 
   constructor(  private router: Router,
                 private busquedaService: BusquedaService,
@@ -26,6 +29,8 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
     this.logged = JSON.parse(localStorage.getItem('user'));
     this.buscarprofile();
+    this.buscarCompras();
+    this.buscarCuota();
   }
 
   muestraModal( content ) {
@@ -46,12 +51,43 @@ export class ProfileComponent implements OnInit {
     );
   }
 
+  buscarFirmas() {
+    const datos = { tipo: 'firmas', usuario: this.logged };
+    this.busquedaService.obtenerDatos(JSON.stringify(datos),
+      data => {
+      this.cantidades = data;
+        setTimeout(() => { this.loadingIndicator = false; }, 1500);
+      }
+    );
+  }
+
+  buscarCuota() {
+    const datos = { tipo: 'cuota', usuario: this.logged };
+    this.cuota$ = this.busquedaService.obtenerCuota(JSON.stringify(datos));
+
+  }
+
+  buscarCompras() {
+    const datos = { tipo: 'ultimasCompras', usuario: this.logged };
+    this.busquedaService.obtenerDatos(JSON.stringify(datos),
+      data => {
+      this.compras = data;
+        setTimeout(() => { this.loadingIndicator = false; }, 1500);
+      }
+    );
+  }
+
 }
 
 interface Dato {
   estado: string;
   valor: string;
 
+}
+
+interface Compra {
+  cantidad: number;
+  cuando: Date;
 }
 
 interface Clave {
